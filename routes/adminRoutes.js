fafdd3b80e13a8f5d8a92c570845d591ec997b2d
Fred S. Silva <indesconectavel@gmail.com>
2025-08-02 19:02:07 -0300

    fix: corrigido caminho do middleware de autenticação

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 const exportController = require('../controllers/exportController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // Relatórios e estatísticas
 router.post('/relatorio-semanal', authMiddleware, adminController.relatorioSemanal);
